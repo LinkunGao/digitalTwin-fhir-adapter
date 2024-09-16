0c09a9e22b0c0e1b4823b2750c14c968d661f2c3
LinkunGao <gaolinkun123@gmail.com>
2024-09-16 12:38:43 +1200

    update version

--- digitaltwins_on_fhir/__init__.py
+++ digitaltwins_on_fhir/__init__.py
@@ -1,7 +1,7 @@
 from .core import Adapter
 
 __title__ = "digitaltwins-on-fhir"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __author__ = "Linkun Gao"
 __license__ = "Apache-2.0"
 __copyright__ = "Copyright 2024 ABI"
